42d54845dca71843bd2d91b4f70cb8797ab199e4
Wilson <wchun108@gmail.com>
2017-10-18 12:56:21 -0700

    Revised W3D3 HW

--- Week 3/W3D2/db/migrate/20171018061922_house_table.rb
+++
@@ -1,9 +0,0 @@
-class HouseTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :house do |t|
-      t.string :address
-
-      t.timestamps
-    end 
-  end
-end
--- Week 3/W3D3/config/application.rb
+++ Week 3/W3D3/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module W3D1
+module W3D2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
