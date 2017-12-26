2e5e1f765a08a37deb7c5e2545989c36bb85ec6e
Wilson <wchun108@gmail.com>
2017-12-25 22:39:47 -0800

    Complete dynamic_array and static_array

--- dynamic_arrays/lib/dynamic_array.rb
+++ dynamic_arrays/lib/dynamic_array.rb
@@ -4,31 +4,57 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @length = 0
+    @capacity = 8
+    @store = StaticArray.new(@capacity)
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    check_index(index)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if @length == 0
+    @length -= 1
+    @store[length]
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length >= @capacity
+    @store[@length] = val
+    @length += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if @length == 0
+    (1..@length).each do |i|
+      @store[i] = @store[i - 1]
+    end
+    @length -= 1
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    temp = StaticArray.new(@capacity)
+    (0...@length).each do |i|
+      temp[i + 1] = @store[i]
+    end
+    temp[0] = val
+    resize! if @length >= @capacity
+    @length += 1
+    @store = temp
   end
 
   protected
@@ -36,9 +62,11 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" if index > @length - 1
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    @capacity = @capacity * 2
   end
 end
