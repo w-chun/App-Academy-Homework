11c6645cebc7853d1bea682d4aa05fd535920232
Wilson <wchun108@gmail.com>
2017-12-27 22:46:01 -0800

    Complete p02 of lru_cache

--- lru_cache/lib/p02_hashing.rb
+++ lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    self.join("").to_i.hash
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).join("").to_i.hash
   end
 end
 
@@ -16,6 +18,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    data = ""
+    self.each { |key, value| data += key.to_s + value.to_s}
+    data.chars.sort.join.hash
   end
 end
