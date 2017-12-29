b1b6e8d6eaf45811711c792806b44022775745cb
Wilson <wchun108@gmail.com>
2017-12-28 20:57:48 -0800

    Complete p04 of lru_cache

--- lru_cache/lib/p04_linked_list.rb
+++ lru_cache/lib/p04_linked_list.rb
@@ -15,11 +15,24 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    next_node = @next
+    prev_node = @prev
+    next_node.prev = prev_node
+    prev_node.next = next_node
+    @next = nil
+    @prev = nil
+    self
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +41,66 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    temp_node = Node.new(key, val)
+    @tail.prev.next = temp_node
+    temp_node.prev = @tail.prev
+    temp_node.next = @tail
+    @tail.prev = temp_node
+    temp_node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    remove_node = nil
+    self.each do |node|
+      remove_node = node if node.key == key
+    end
+    if remove_node
+      remove_node.remove
+    end
   end
 
   def each
+    temp_node = @head.next
+    until temp_node == @tail
+      yield temp_node
+      temp_node = temp_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end
