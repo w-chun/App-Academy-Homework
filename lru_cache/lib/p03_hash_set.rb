d96b349e28b03502744445cb7ff77c9ad35f3341
Wilson <wchun108@gmail.com>
2017-12-28 17:25:24 -0800

    Complete p03 of lru_cache

--- lru_cache/lib/p03_hash_set.rb
+++ lru_cache/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    unless self.include?(key)
+      @count += 1
+      if @count > num_buckets
+        resize!
+      end
+      self[key].push(key)
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,13 @@ class HashSet
   end
 
   def resize!
+    new_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 1
+    new_store.each do |bucket|
+      bucket.each do |el|
+        self.insert(el)
+      end
+    end
   end
 end
