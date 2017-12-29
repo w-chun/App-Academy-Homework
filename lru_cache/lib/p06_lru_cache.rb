496b34576b35696f98f7747ac3ee7a0baeef1f79
Wilson <wchun108@gmail.com>
2017-12-29 13:54:11 -0800

    Complete p06 of lru_cache

--- lru_cache/lib/p06_lru_cache.rb
+++ lru_cache/lib/p06_lru_cache.rb
@@ -15,6 +15,11 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      update_node!(@map[key]).val
+    else
+      calc!(key).val
+    end
   end
 
   def to_s
@@ -25,12 +30,18 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    eject! if count == @max
+    @map[key] = @store.append(key, @prc.call(key))
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    updated_node = node.remove
+    @map[updated_node.key] = @store.append(updated_node.key, updated_node.val)
   end
 
   def eject!
+    @map.delete(@store.first.key)
+    @store.first.remove
   end
 end
