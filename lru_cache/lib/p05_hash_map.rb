bd401c3dfebcda12fa510e97d15df74a4edc5187
Wilson <wchun108@gmail.com>
2017-12-29 12:29:27 -0800

    Complete p05 of lru_cache

--- lru_cache/lib/p05_hash_map.rb
+++ lru_cache/lib/p05_hash_map.rb
@@ -3,6 +3,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_reader :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -10,27 +11,45 @@ class HashMap
   end
 
   def include?(key)
+    @store[bucket(key)].include?(key)
   end
 
   def set(key, val)
+    if self.include?(key)
+      @store[bucket(key)].update(key, val)
+    else
+      @count += 1
+      if count > num_buckets
+        resize!
+      end
+      @store[bucket(key)].append(key, val)
+    end
   end
 
   def get(key)
+    @store[bucket(key)].get(key)
   end
 
   def delete(key)
+    @store[bucket(key)].remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |link|
+        yield [link.key, link.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,9 +61,18 @@ class HashMap
   end
 
   def resize!
+    new_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 1
+    new_store.each do |bucket|
+      bucket.each do |link|
+        self.set(link.key, link.val)
+      end
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
 end
