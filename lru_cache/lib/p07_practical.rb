922522d85c6e58ce3a11dbfb207b462d6004634b
Wilson <wchun108@gmail.com>
2017-12-30 21:39:35 -0800

    Complete lru_cache

--- lru_cache/lib/p07_practical.rb
+++ lru_cache/lib/p07_practical.rb
@@ -1,5 +1,20 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  map = HashMap.new()
+  string.chars.each do |letter|
+    if map.include?(letter)
+      map[letter] += 1
+    else
+      map[letter] = 1
+    end
+  end
+
+  nums = 0
+  map.each do |link, val|
+    if val.odd?
+      nums += 1
+    end
+  end
+  nums <= 1
 end
