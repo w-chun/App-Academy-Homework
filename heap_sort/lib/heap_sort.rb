c463e5a58b7ba77d38c5beec0cc168ec8f6ba6e2
Wilson <wchun108@gmail.com>
2018-01-04 09:35:06 -0800

    Complete heap_sort

--- heap_sort/lib/heap_sort.rb
+++ heap_sort/lib/heap_sort.rb
@@ -2,5 +2,12 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    arr = BinaryMinHeap.new
+    self.length.times do
+      arr.push(self.shift)
+    end
+    until arr.store.count == 0
+      self.push(arr.extract)
+    end
   end
 end
--- heap_sort/lib/k_largest_elements.rb
+++ heap_sort/lib/k_largest_elements.rb
@@ -1,5 +1,16 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
+  sorted = []
+  arr = BinaryMinHeap.new
 
+  array.each do |num|
+    arr.push(num)
+  end
+
+  until arr.count == 0
+    sorted.unshift(arr.extract)
+  end
+
+  sorted[0...k]
 end
