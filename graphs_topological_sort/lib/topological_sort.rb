16235b0839832c3ac155e7ca4629b152a2816bd6
Wilson <wchun108@gmail.com>
2018-01-09 16:04:59 -0800

    Add Tarjan's algorithm

--- graphs_topological_sort/lib/topological_sort.rb
+++ graphs_topological_sort/lib/topological_sort.rb
@@ -1,29 +1,52 @@
 require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
+#
+# def topological_sort(vertices)
+#   sorted = []
+#   top = []
+#   vertices.each do |vertex|
+#     if vertex.in_edges.empty?
+#       top.push(vertex)
+#     end
+#   end
+#
+#   until top.empty?
+#     current = top.pop
+#     sorted << current
+#
+#     current.out_edges.each do |edge|
+#       to_vertex = edge.to_vertex
+#       edge.to_vertex.in_edges.delete(edge)
+#       if to_vertex.in_edges.empty?
+#         top.push(to_vertex)
+#       end
+#     end
+#     vertices.delete(current)
+#   end
+#
+#   vertices.empty? ? sorted : []
+# end
 
+#--------------Tarjan's Algorithm----------
 def topological_sort(vertices)
-  sorted = []
-  top = []
-  vertices.each do |vertex|
-    if vertex.in_edges.empty?
-      top.push(vertex)
-    end
+  ordering = []
+  explored = Set.new
+
+  vertices.each do |vertex| # O(|v|)
+    dfs!(vertex, explored, ordering) unless explored.include?(vertex)
   end
 
-  until top.empty?
-    current = top.pop
-    sorted << current
+  ordering
+end
 
-    current.out_edges.each do |edge|
-      to_vertex = edge.to_vertex
-      edge.to_vertex.in_edges.delete(edge)
-      if to_vertex.in_edges.empty?
-        top.push(to_vertex)
-      end
-    end
-    vertices.delete(current)
+def dfs!(vertex, explored, ordering)
+  explored.add(vertex)
+
+  vertex.out_edges.each do |edge| # O(|e|)
+    new_vertex = edge.to_vertex
+    dfs!(new_vertex, explored, ordering) unless explored.include?(new_vertex)
   end
 
-  vertices.empty? ? sorted : []
+  ordering.unshift(vertex)
 end
